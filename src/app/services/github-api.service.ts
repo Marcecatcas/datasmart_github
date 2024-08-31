1a79381db6b433ebd0a0bfc7b415df7116196fb9
Marcecatcas <marce.catalan.castelli@gmail.com>
2024-08-31 17:21:00 -0400

    se agrega environment para manejar la conexion con la api de github

--- src/app/services/github-api.service.ts
+++ src/app/services/github-api.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubApiService {
-  private apiUrl = 'https://api.github.com';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
